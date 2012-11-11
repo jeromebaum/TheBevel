100e47659234eb874a742d3fb48cdad2b036044e
Jerome Baum <jerome@jeromebaum.com>
2012-11-11 21:08:26 +0100

    Revamp how the UI works, now follows mini instructions

--- calc.js
+++ calc.js
@@ -1,86 +1,82 @@
 /* Yes, this is ugly code. It gets the job done. */
 
 function CalcCtrl($scope) {
-	$scope.leftNumbers = [{v:4}, {v:3}, {v:''}];
-	$scope.rightNumbers = [{v:1024}, {v:768}, {v:''}];
-
 	$scope.commonRatios = [
 		{
-			name: '1024x768',
-			left: [{v:1024}, {v:768}, {v:''}],
-			right: [{v:800}, {v:600}, {v:''}]
+			name: '4:3',
+			left: [4, 3],
+			rightFirst: 1024
 		},
 		{
 			name: 'Golden ratio',
-			left: [{v:1000}, {v:1618}, {v:''}],
-			right: [{v:800}, {v:1618*800/1000}, {v:''}]
+			left: [1, 1.618],
+			rightFirst: 800
 		},
 		{
 			name: '1, 2, 4, ..., 2⁸',
-			left: [{v:1}, {v:2}, {v:4}, {v:8}, {v:16}, {v:32}, {v:64}, {v:128}, {v:256}, {v:''}],
-			right: [{v:1}, {v:2}, {v:4}, {v:8}, {v:16}, {v:32}, {v:64}, {v:128}, {v:256}, {v:''}]
+			left: [1, 2, 4, 8, 16, 32, 64, 128, 256],
+			rightFirst: 3
 		}
 	];
+	$scope.rightNumbers = [];
 
-	var leftFirst = $scope.leftNumbers[0].v;
-	var rightFirst = $scope.rightNumbers[0].v;
-
-	$scope.update = function(side, index) {
-		var itemCount = $scope.leftNumbers.length;
-		if (index === 0) {
-			// base ratio change
-			// change all other items on our side
-			if (side === 'left') {
-				var multiplier = Math.max($scope.leftNumbers[0].v, 1) / leftFirst;
-				// yes, exclude first and last!
-				for (var i = 1; i < itemCount - 1; i++) {
-					$scope.leftNumbers[i].v *= multiplier;
-				}
-			}
-			if (side === 'right') {
-				var multiplier = Math.max($scope.rightNumbers[0].v, 1) / rightFirst;
-				// yes, exclude first and last!
-				for (var i = 1; i < itemCount - 1; i++) {
-					$scope.rightNumbers[i].v *= multiplier;
-				}
-			}
-		} else {
-			if (index === itemCount - 1) {
-				// new-item set
-				// add another new-item
-				$scope.leftNumbers[itemCount] = {v:''};
-				$scope.rightNumbers[itemCount] = {v:''};
-			} else {
-				// if set to 0, remove this row
-				if ((side === 'left' && $scope.leftNumbers[index].v === 0) ||
-					(side === 'right' && $scope.rightNumbers[index].v === 0)) {
-					$scope.leftNumbers.splice(index, 1);
-					$scope.rightNumbers.splice(index, 1);
-				}
-			}
-			// item change
-			// change corresponding item on other side
-			if (side === 'left') {
-				var multiplier = rightFirst / leftFirst;
-				$scope.rightNumbers[index].v = $scope.leftNumbers[index].v;
-				$scope.rightNumbers[index].v *= multiplier;
-			}
-			if (side === 'right') {
-				var multiplier = leftFirst / rightFirst;
-				$scope.leftNumbers[index].v = $scope.rightNumbers[index].v;
-				$scope.leftNumbers[index].v *= multiplier;
-			}
+	var populateRight = function(scale) {
+		var left = $scope.leftNumbers;
+		var length = left.length;
+		var right = $scope.rightNumbers;
+		right.splice(length - 1, right.length - length + 1);
+		for (var i = 0; i < length; i++) {
+			if (i === length - 1 && left[i].v === '') continue;
+			var newV = left[i].v*scale;
+			if (i < right.length && right[i].v === newV) continue;
+			right[i] = {v: newV};
 		}
-		// update *First
-		leftFirst = Math.max($scope.leftNumbers[0].v, 1);
-		rightFirst = Math.max($scope.rightNumbers[0].v, 1);
 	}
 
 	$scope.useRatio = function(i) {
 		var ratio = $scope.commonRatios[i];
-		$scope.leftNumbers = ratio.left;
-		$scope.rightNumbers = ratio.right;
-		leftFirst = ratio.left[0].v;
-		rightFrist = ratio.right[0].v;
+		var left = ratio.left;
+		var length = left.length;
+		$scope.leftNumbers = [];
+		for (var i = 0; i < length; i++) {
+			$scope.leftNumbers[i] = {v: left[i]};
+		}
+		$scope.leftNumbers[length] = {v: ''};
+		var scale = ratio.rightFirst / left[0];
+		populateRight(scale);
+	}
+
+	$scope.useRatio(0);
+
+	var removeEmptyItems = function(left) {
+		var length = left.length;
+		for (var i = length-1; i--;) {
+			var v = left[i].v;
+			if (v === '' || v === 0) {
+				left.splice(i, 1);
+			}
+		}
+	}
+
+	var addNewItem = function(left) {
+		var length = left.length;
+		if (left[length-1].v !== '') {
+			left[length] = {v: ''};
+		}
+	}
+
+	$scope.update = function(side, i) {
+		if (side === 'right') {
+			var leftNth = $scope.leftNumbers[i].v;
+			var rightNth = $scope.rightNumbers[i].v;
+			var scale = rightNth / leftNth;
+			populateRight(scale, i);
+			return;
+		}
+		// now side === 'left'
+		var left = $scope.leftNumbers;
+		removeEmptyItems(left);
+		addNewItem(left);
+		$scope.update('right', 0);
 	}
 }
